93d6f706b4239a08b8c4355809509bd14cad027c
Dorian ROUX <mp.rouxdorian@gmail.com>
2023-04-28 11:12:10 +0200

    Revert "fix secret path"
    This reverts commit e5577d1f2298038cab196b7a8edffb9fe442ac9f.

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def main():
     # -- STEP 1 - Initial the Interface + Variables --
     
     # --- Setup the Paths ---
-    configPath = os.path.join(os.path.dirname(__file__), '.streamlit', 'secrets.toml')
+    configPath = '.streamlit/secrets.toml'
     staticPath = os.path.join(os.path.dirname(__file__), 'src', 'static')
     pathImages_logo = os.path.join(staticPath, 'images', 'iconRelease.png')
     
